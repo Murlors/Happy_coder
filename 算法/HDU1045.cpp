d5d08580c5aeb0f5b8f919803e8b02ab080ea2b7
alex <1595258509@qq.com>
2022-04-27 23:26:58 +0800

    HDU1045.

---
+++ 算法/HDU1045.cpp
@@ -0,0 +1,47 @@
+/*
+URL:http://acm.hdu.edu.cn/showproblem.php?pid=1045
+完成日期:2022.4.27
+所用方法:暴力深搜
+*/
+#include "bits/stdc++.h"
+using namespace std;
+int n, ma;
+char mp[5][5];
+bool judge(int i, int j) {
+    for (int k = i; k > 0; --k) {
+        if (mp[k][j] == 'X')break;
+        if (mp[k][j] == 'O')return false;
+    }
+    for (int k = j; k > 0; --k) {
+        if (mp[i][k] == 'X')break;
+        if (mp[i][k] == 'O')return false;
+    }
+    return true;
+}
+void dfs(int i, int j, int cnt) {
+    if (i == n && j == n) {
+        if (mp[i][j] == '.' && judge(i, j)) cnt++;
+        if (cnt > ma) ma = cnt;
+        return;
+    } else {
+        if (mp[i][j] == '.' && judge(i, j)) {
+            mp[i][j] = 'O';
+            if (j == n)dfs(i + 1, 1, cnt + 1);
+            else dfs(i, j + 1, cnt + 1);
+            mp[i][j] = '.';
+        }
+        if (j == n)dfs(i + 1, 1, cnt);
+        else dfs(i, j + 1, cnt);
+    }
+}
+int main() {
+    while (cin >> n, n) {
+        ma = 0;
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= n; ++j)
+                cin >> mp[i][j];
+        dfs(1, 1, 0);
+        cout << ma << "\n";
+    }
+    return 0;
+}
